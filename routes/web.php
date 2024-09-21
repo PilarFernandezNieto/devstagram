773728c98524f0207a88dfbf07496a3a3c2c7a0c
PilarFernandezNieto <apreilmc@gmail.com>
2024-09-21 13:14:58 +0200

    Vista principal

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('principal');
 });
-Route::get('/nosotros', function () {
-    return view('nosotros');
-});
+
