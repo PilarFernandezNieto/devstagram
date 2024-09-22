f0538ebbf56b1b144a73daf5211ad94732ce257f
PilarFernandezNieto <apreilmc@gmail.com>
2024-09-22 20:58:49 +0200

    Crea comentarios y lanza mensaje de confirmación

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}" class="rounded">
             <div class="py-3">
@@ -18,11 +18,17 @@
                 <p class="mt-5">{{ $post->descripcion }}</p>
             </div>
         </div>
-        <div class="md:w-1/2 p-5">
-            <div class="shadow bg-white p-5 mb-5">
+        <div class="md:w-1/2 md:p-5">
+            <div class="shadow bg-white p-5 mb-5 rounded">
                 @auth
                     <p class="text-xl font-bold text-center mb-4">Agrega un nuevo comentario</p>
-                    <form action="">
+                    @if (session('mensaje'))
+                        <p class="bg-green-500 text-white rounded p-2 uppercase font-bold text-center my-3 text-sm">{{ session('mensaje') }}
+                        </p>
+                    @endif
+
+                    <form action="{{ route('comentarios.store', ['user' => $user, 'post' => $post]) }}" method="POST">
+                        @csrf
                         <div class="mb-5 ">
                             <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold ">Comentario</label>
                             <textarea id="comentario" name="comentario" placeholder="Agrega tu comentario"
