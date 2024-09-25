51accc4786534162e4479559f92e218997897126
PilarFernandezNieto <apreilmc@gmail.com>
2024-09-25 20:25:38 +0200

    componente para listar posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,10 +27,10 @@
                     @endauth
                 </div>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
-                    {{$user->followers->count()}} <span class="font-normal">@choice('Seguidor|Seguidores', $user->followers->count())</span>
+                    {{ $user->followers->count() }} <span class="font-normal">@choice('Seguidor|Seguidores', $user->followers->count())</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                    {{$user->followings->count()}} <span class="font-normal">Siguiendo</span>
+                    {{ $user->followings->count() }} <span class="font-normal">Siguiendo</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
                     {{ $user->posts->count() }} <span class="font-normal">Posts</span>
@@ -62,23 +62,6 @@
     </div>
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-
-        @if ($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach ($posts as $post)
-                    <div>
-                        <a href="{{ route('posts.show', ['user' => $post->user, 'post' => $post]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}"
-                                alt="Imagen del post {{ $post->titulo }}" class="rounded">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            <div class="my-10">
-                {{ $posts->links('pagination::tailwind') }}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-sm text-center font-bold">Todavía no hay publicaciones</p>
-        @endif
+        <x-listar-post :posts="$posts"/>
     </section>
 @endsection
